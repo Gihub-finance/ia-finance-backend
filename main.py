05c20ceec99520d341fa2622de4bd393fd56346f
Gihub-finance <danielduret33@protonmail.com>
2026-06-08 20:51:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 import os
 import requests
 from openai import OpenAI
-from dotenv import load_dotenv
+from dotenv import load_dotenv 
 from datetime import datetime, timedelta
 
 load_dotenv()
